60a88ac5c2f7461fe83347be83b0e4033f3dfbea
C. Scott Ananian <cscott@cscott.net>
2021-06-18 13:27:56 -0400

    Use "I2C Sender" example
    From https://www.arduino.cc/en/Tutorial/LibraryExamples/MasterReader

--- esp32-rainbarrel/src/flowmeter.cpp
+++ esp32-rainbarrel/src/flowmeter.cpp
@@ -1,26 +1,34 @@
 #ifdef FLOWMETER
 #include "Arduino.h"
+// Wire Slave Sender
+// by Nicholas Zambetti <http://www.zambetti.com>
 
-void setup()
-{
-    // Set Pins 0 and 1 as outputs.
-    // Some Digisparks have a built-in LED on pin 0, while some have it on
-    // pin 1. This way, we can all Digisparks.
-    pinMode(0, OUTPUT);
-    pinMode(1, OUTPUT);
+// Demonstrates use of the Wire library
+// Sends data as an I2C/TWI slave device
+// Refer to the "Wire Master Reader" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+#include <Wire.h>
+
+void requestEvent();
+
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
 }
 
-void loop()
-{
-    // Set the LED pins to HIGH. This gives power to the LED and turns it on
-    digitalWrite(0, HIGH);
-    digitalWrite(1, HIGH);
-    // Wait for a second
-    delay(1000);
-    // Set the LED pins to LOW. This turns it off
-    digitalWrite(0, LOW);
-    digitalWrite(1, LOW);
-    // Wait for a second
-    delay(500);
+void loop() {
+  delay(100);
 }
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write("hello "); // respond with message of 6 bytes
+  // as expected by master
+}
+
 #endif /* FLOWMETER */
